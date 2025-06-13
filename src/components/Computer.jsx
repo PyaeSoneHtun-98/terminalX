9894fdd60f8c57a777351e0e15467292b3eb2613
PyaeSoneHtun-98 <123477615+PyaeSoneHtun-98@users.noreply.github.com>
2025-06-14 01:54:12 +0630

    (updae) loading method again

--- src/components/Computer.jsx
+++ src/components/Computer.jsx
@@ -3,11 +3,10 @@ import { useEffect, useState } from 'react'
 import * as THREE from 'three'
 import { useSpring, animated } from '@react-spring/three'
 
-export function Computer() {
+export function Computer({ onLoad }) {
   const computer = useGLTF('/assets/terminalx-optimized.glb')
   const { scene, animations } = computer
-
-  const [scale, setScale] = useState(1.5) // Default scale
+  const [scale, setScale] = useState(1.5)
   const [positionX, setPositionX] = useState(1.2)
   const [positionY, setPositionY] = useState(0.5)
 
@@ -56,6 +55,12 @@ export function Computer() {
     }
   }, [scene, animations])
 
+  useEffect(() => {
+    if (scene) {
+      onLoad?.()
+    }
+  }, [scene, onLoad])
+
   return (
     <animated.primitive 
       object={scene}
@@ -63,7 +68,7 @@ export function Computer() {
       position-x={positionX}
       position-y={spring.y}
       position-z={0}
-      rotation={[0, -Math.PI / 2 - 0.05, 0]} // ~ -85 degrees
+      rotation={[0, -Math.PI / 2 - 0.05, 0]}
     />
   )
 }
